42097f60502e2fd61e56a829eba289f0d15eaf95
DarkNight <42426324@qq.com>
2018-04-13 17:24:47 +0800

    优化
    优化

--- app/src/main/java/cn/lizhiyu/closeeye/activity/RegisterActivity.java
+++ app/src/main/java/cn/lizhiyu/closeeye/activity/RegisterActivity.java
@@ -11,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import cn.lizhiyu.closeeye.R;
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -67,8 +69,20 @@ public class RegisterActivity extends AppCompatActivity {
 
         textViewGetCode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
+                // 注册一个事件回调，用于处理发送验证码操作的结果
+                SMSSDK.registerEventHandler(new EventHandler() {
+                    public void afterEvent(int event, int result, Object data) {
+                        if (result == SMSSDK.RESULT_COMPLETE) {
+                            // TODO 处理成功得到验证码的结果
+                            // 请注意，此时只是完成了发送验证码的请求，验证码短信还需要几秒钟之后才送达
+                        } else{
+                            // TODO 处理错误的结果
+                        }
 
+                    }
+                });
             }
         });
     }
