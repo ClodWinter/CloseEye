abbe2603e30bff4bf2c8499e854500276ec51c40
DarkNight <42426324@qq.com>
2018-04-26 10:44:39 +0800

    优化
    优化

--- app/src/main/java/cn/lizhiyu/closeeye/activity/ForgetPasswordActivity.java
+++ app/src/main/java/cn/lizhiyu/closeeye/activity/ForgetPasswordActivity.java
@@ -44,147 +44,147 @@ public class ForgetPasswordActivity extends AppCompatActivity {
             window.setNavigationBarColor(Color.TRANSPARENT);
         }
 
-//        setContentView(R.layout.activity_forget_password);
+        setContentView(R.layout.activity_forget_password);
 
         createView();
     }
 
     private void createView()
     {
-//        ImageView imageViewClose = (ImageView) findViewById(R.id.activity_forget_close);
-//
-//        imageViewClose.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                finish();
-//            }
-//        });
-//
-//
-//        final EditText editTextAccount = (EditText) findViewById(R.id.forget_account_edittext);
-//
-//        final EditText editTextPass = (EditText) findViewById(R.id.forget_pass_edittext);
-//
-//        final EditText editTextCode = (EditText) findViewById(R.id.forget_vali_edittext);
-//
-//        final EditText editTextPassAgain = (EditText) findViewById(R.id.forget_again_pass_edittext);
-//
-//        final ImageView imageViewAction = (ImageView) findViewById(R.id.forget_button);
-//
-//        final TextView textViewGetCode = (TextView) findViewById(R.id.forget_getValiCode);
-//
-//
-//        imageViewAction.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v)
-//            {
-//
-//            }
-//        });
-//
-//        textViewGetCode.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v)
-//            {
-//
-//                if (editTextAccount.getText().length() == 0)
-//                {
-//                    Toast.makeText(ForgetPasswordActivity.this,"手机号码不能为空",Toast.LENGTH_LONG).show();
-//
-//                    return;
-//                }
-//
-//                if (!CommonTool.isMobile(editTextAccount.getText().toString()))
-//                {
-//                    Toast.makeText(ForgetPasswordActivity.this,"手机号码格式不正确",Toast.LENGTH_LONG).show();
-//
-//                    return;
-//                }
-//
-//                // 注册一个事件回调，用于处理发送验证码操作的结果
-//                SMSSDK.registerEventHandler(new EventHandler() {
-//                    public void afterEvent(int event, int result, Object data) {
-//                        if (result == SMSSDK.RESULT_COMPLETE) {
-//                            // TODO 处理成功得到验证码的结果
-//                            // 请注意，此时只是完成了发送验证码的请求，验证码短信还需要几秒钟之后才送达
-//                        } else{
-//                            // TODO 处理错误的结果
-//                        }
-//
-//                    }
-//                });
-//
-//                // 触发操作
-//                SMSSDK.getVerificationCode("86", editTextAccount.getText().toString());
-//
-//                Toast.makeText(ForgetPasswordActivity.this,"验证码已发送，请注意查收。",Toast.LENGTH_LONG).show();
-//
-//                textViewGetCode.setClickable(false);
-//
-//                final CountDownTimer timer = new CountDownTimer(60000,1000) {
-//                    @Override
-//                    public void onTick(long t)
-//                    {
-//                        textViewGetCode.setText(t/1000+" 后重发");
-//                    }
-//
-//                    @Override
-//                    public void onFinish()
-//                    {
-//                        textViewGetCode.setText("获取验证码");
-//
-//                        textViewGetCode.setClickable(true);
-//                    }
-//                }.start();
-//            }
-//        });
-//
-//        final Button buttonCheck = (Button) findViewById(R.id.register_check);
-//
-//        buttonCheck.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                buttonCheck.setSelected(!buttonCheck.isSelected());
-//
-//                if (buttonCheck.isSelected())
-//                {
-//                    buttonCheck.setBackgroundResource(R.drawable.check_box);
-//                }else
-//                {
-//                    buttonCheck.setBackgroundResource(R.drawable.uncheck_box);
-//                }
-//            }
-//        });
-//
-//        TextWatcher textWatcher = new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after)
-//            {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                if (editTextAccount.getText().length()>0 && editTextCode.getText().length()>0 && editTextPass.getText().length()>0 && editTextPassAgain.getText().length()>0)
-//                {
-//                    imageViewAction.setBackgroundColor(Color.parseColor("#f34649"));
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//
-//            }
-//        };
-//
-//        editTextAccount.addTextChangedListener(textWatcher);
-//
-//        editTextCode.addTextChangedListener(textWatcher);
-//
-//        editTextPass.addTextChangedListener(textWatcher);
-//
-//        editTextPassAgain.addTextChangedListener(textWatcher);
+        ImageView imageViewClose = (ImageView) findViewById(R.id.activity_forget_close);
+
+        imageViewClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+
+
+        final EditText editTextAccount = (EditText) findViewById(R.id.forget_account_edittext);
+
+        final EditText editTextPass = (EditText) findViewById(R.id.forget_pass_edittext);
+
+        final EditText editTextCode = (EditText) findViewById(R.id.forget_vali_edittext);
+
+        final EditText editTextPassAgain = (EditText) findViewById(R.id.forget_again_pass_edittext);
+
+        final ImageView imageViewAction = (ImageView) findViewById(R.id.forget_button);
+
+        final TextView textViewGetCode = (TextView) findViewById(R.id.forget_getValiCode);
+
+
+        imageViewAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        textViewGetCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                if (editTextAccount.getText().length() == 0)
+                {
+                    Toast.makeText(ForgetPasswordActivity.this,"手机号码不能为空",Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+
+                if (!CommonTool.isMobile(editTextAccount.getText().toString()))
+                {
+                    Toast.makeText(ForgetPasswordActivity.this,"手机号码格式不正确",Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+
+                // 注册一个事件回调，用于处理发送验证码操作的结果
+                SMSSDK.registerEventHandler(new EventHandler() {
+                    public void afterEvent(int event, int result, Object data) {
+                        if (result == SMSSDK.RESULT_COMPLETE) {
+                            // TODO 处理成功得到验证码的结果
+                            // 请注意，此时只是完成了发送验证码的请求，验证码短信还需要几秒钟之后才送达
+                        } else{
+                            // TODO 处理错误的结果
+                        }
+
+                    }
+                });
+
+                // 触发操作
+                SMSSDK.getVerificationCode("86", editTextAccount.getText().toString());
+
+                Toast.makeText(ForgetPasswordActivity.this,"验证码已发送，请注意查收。",Toast.LENGTH_LONG).show();
+
+                textViewGetCode.setClickable(false);
+
+                final CountDownTimer timer = new CountDownTimer(60000,1000) {
+                    @Override
+                    public void onTick(long t)
+                    {
+                        textViewGetCode.setText(t/1000+" 秒后重发");
+                    }
+
+                    @Override
+                    public void onFinish()
+                    {
+                        textViewGetCode.setText("获取验证码");
+
+                        textViewGetCode.setClickable(true);
+                    }
+                }.start();
+            }
+        });
+
+        final Button buttonCheck = (Button) findViewById(R.id.register_check);
+
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                buttonCheck.setSelected(!buttonCheck.isSelected());
+
+                if (buttonCheck.isSelected())
+                {
+                    buttonCheck.setBackgroundResource(R.drawable.check_box);
+                }else
+                {
+                    buttonCheck.setBackgroundResource(R.drawable.uncheck_box);
+                }
+            }
+        });
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (editTextAccount.getText().length()>0 && editTextCode.getText().length()>0 && editTextPass.getText().length()>0 && editTextPassAgain.getText().length()>0)
+                {
+                    imageViewAction.setBackgroundColor(Color.parseColor("#f34649"));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        editTextAccount.addTextChangedListener(textWatcher);
+
+        editTextCode.addTextChangedListener(textWatcher);
+
+        editTextPass.addTextChangedListener(textWatcher);
+
+        editTextPassAgain.addTextChangedListener(textWatcher);
     }
 }
